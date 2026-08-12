b4bcef030a42eccef56fa52fc2f62a2fe04b0114
Mooling0602 <clemooling@outlook.com>
2026-08-12 17:40:26 +0800

    fix: update import paths

--- src/modern_teleport/__init__.py
+++ src/modern_teleport/__init__.py
@@ -5,10 +5,14 @@ About docstring and i18n, see [CODESTYLE.md](doc/CODESTYLE.md).
 
 from mcdreforged.api.all import PluginServerInterface
 
-from modern_teleport.utils import tr
+import modern_teleport.runtime as rt
+from modern_teleport.config import get_config
+from modern_teleport.config.i18n import tr
 
 
 def on_load(server: PluginServerInterface, _):
+    rt.psi = server
+    rt.config = get_config(server)
     server.logger.info(tr(server, "loading_message"))
 
 
